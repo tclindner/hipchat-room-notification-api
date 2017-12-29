32e29fe506e433424ce6606b3811e9a9fb28de4b
tclindner <tclindner@users.noreply.github.com>
2017-12-28 18:23:42 -0600

    Resolve ESLint errors

--- test/networkRequest.test.js
+++ test/networkRequest.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const chai = require('chai');
-const chaiAsPromised = require("chai-as-promised");
+const chaiAsPromised = require('chai-as-promised');
 const sinon = require('sinon');
 const HipChatRoomNotification = require('./../src/HipChatRoomNotification');
 
@@ -27,62 +27,46 @@ describe('HipChatRoomNotification Network Unit Tests', function() {
     notification = new HipChatRoomNotification('https://www.example.com', '1', 'abcd1234');
   });
 
-  afterEach(function() {
-    request.get.restore();
-  });
-
-  it("successful post - basic message", function() {
+  it('successful post - basic message', function() {
     notification.setMessage('test');
 
-    server.respond(
-      204,
-      {},
-      ''
-    );
+    server.respond(204, {}, '');
 
     const promise = notification.send();
 
     return promise.should.eventually.equal('successfully posted to hipchat');
   });
 
-  it("successful post - card", function() {
+  it('successful post - card', function() {
     notification.setMessage('message');
     notification.addCard('1', 'file', 'title');
 
-    server.respond(
-      204,
-      {},
-      ''
-    );
+    server.respond(204, {}, '');
 
     const promise = notification.send();
 
     return promise.should.eventually.equal('successfully posted to hipchat');
   });
 
-  it("bad post", function() {
-    server.respond(
-      400,
-      {},
-      ''
-    );
+  it('bad post', function() {
+    server.respond(400, {}, '');
 
     const promise = notification.send();
 
     return promise.should.eventually.be.rejected;
   });
 
-  it("invalid basic request object - failed validation", function() {
+  it('invalid basic request object - failed validation', function() {
     const promise = notification.send();
 
-    return expect(promise).to.eventually.equal('successfully posted to hipchat');
+    return promise.should.eventually.equal('successfully posted to hipchat');
   });
 
-  it("invalid card request object - failed validation", function() {
+  it('invalid card request object - failed validation', function() {
     notification.addCard('1', 'file', 'title');
 
     const promise = notification.send();
 
-    return expect(promise).to.eventually.equal('successfully posted to hipchat');
+    return promise.should.eventually.equal('successfully posted to hipchat');
   });
 });
